06c6db0afef825eb7b75b89cbc5acd37f516419d
proxrel <osmanfutbol2000@gmail.com>
2026-09-21 20:25:20 +0300

    Auto: 1 dosya - 2026-09-21 20:25
    app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     applicationId = "io.lunosfer.dreamap"
     minSdk = 24
     targetSdk = 36
-    versionCode = 9
-    versionName = "1.4.0"
+    versionCode = 10
+    versionName = "1.4.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
